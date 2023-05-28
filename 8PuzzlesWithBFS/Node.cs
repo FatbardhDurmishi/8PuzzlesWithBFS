a852e2369c65602049046df74955482026ebccbb
Fatbardh Durmishi <fdurmishi789@gmail.com>
2023-05-28 19:12:25 +0200

    Add project files.

---
+++ 8PuzzlesWithBFS/Node.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8PuzzlesWithBFS
+{
+    public class Node
+    {
+        public int[,] State { get; set; } = new int[3, 3];
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public int Depth { get; set; }
+        public Node Parent { get; set; }
+
+
+
+        public bool IsGoalState(int[,] goalState, int[,] state)
+        {
+            for (int i = 0; i < state.GetLength(0); i++)
+            {
+                for (int j = 0; j < state.GetLength(1); j++)
+                {
+                    if (state[i, j] != goalState[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public List<Node> GetChildren(Node node)
+        {
+            List<Node> nodes = new List<Node>();
+            int row = node.Row;
+            int col = node.Column;
+            Node newNode;
+
+            //move empty tile up
+            //empty tile mundemi me zhvendos nalt kur osht ne rreshtin 1 ose 2
+            if (row > 0)
+            {
+                int[,] newState = (int[,])node.State.Clone();
+                newState[row, col] = node.State[row - 1, col];
+                //0  e konsiderojm si empty tile
+                newState[row - 1, col] = 0;
+                newNode = new Node
+                {
+                    State = newState,
+                    Row = row - 1,
+                    Column = col,
+                    Depth = node.Depth + 1,
+                    Parent = node
+                };
+
+                nodes.Add(newNode);
+            }
+            ////move empty tile down
+            if (row < 2)
+            {
+                int[,] newState = (int[,])node.State.Clone();
+                newState[row, col] = node.State[row + 1, col];
+                newState[row + 1, col] = 0;
+                newNode = new Node
+                {
+                    State = newState,
+                    Row = row + 1,
+                    Column = col,
+                    Depth = node.Depth + 1,
+                    Parent = node
+                };
+                nodes.Add(newNode);
+            }
+
+            //move empty tile left
+            if (col > 0)
+            {
+                int[,] newState = (int[,])node.State.Clone();
+                newState[row, col] = node.State[row, col - 1];
+                newState[row, col - 1] = 0;
+                newNode = new Node
+                {
+                    State = newState,
+                    Row = row,
+                    Column = col - 1,
+                    Depth = node.Depth + 1,
+                    Parent = node
+                };
+                nodes.Add(newNode);
+
+            }
+            //move empty tile right
+            if (col < 2)
+            {
+                int[,] newState = (int[,])node.State.Clone();
+                newState[row, col] = node.State[row, col + 1];
+                newState[row, col + 1] = 0;
+                newNode = new Node
+                {
+                    State = newState,
+                    Row = row,
+                    Column = col + 1,
+                    Depth = node.Depth + 1,
+                    Parent = node
+                };
+                nodes.Add(newNode);
+            }
+            return nodes;
+
+        }
+
+        //funksion qe kontrollon nese dy gjendjet jane te njejta
+        public bool StateIsEqual(Node OtherNode)
+        {
+            for (int i = 0; i < State.GetLength(0); i++)
+            {
+                for (int j = 0; j < State.GetLength(1); j++)
+                {
+                    if (State[i, j] != OtherNode.State[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        //funksion qe kontrollon nese gjendja eshte vizituar
+        public bool StateIsChecked(HashSet<Node> visited)
+        {
+            foreach (Node node in visited)
+            {
+                //this i bje mu kon the current instace e klases
+                if (node.StateIsEqual(this))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+
+}
---
+++ 8PuzzlesWithBFS/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8PuzzlesWithBFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] goalState = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 0 } };
+            Node initialNode = new Node
+            {
+                State = new int[,] {
+                    { 1, 8, 2},
+                    { 0, 4, 3 },
+                    { 7, 6, 5 } },
+                Row = 1,
+                Column = 0,
+                Depth = 0,
+                Parent = null
+
+            };
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(initialNode);
+
+            HashSet<Node> visited = new HashSet<Node>();
+
+            while (queue.Count > 0)
+            {
+                Node current = queue.Dequeue();
+
+                if (current.IsGoalState(goalState, current.State))
+                {
+                    Console.WriteLine("The puzzle is solved" + "\nDepth: " + current.Depth);
+                    for (int i = 0; i < current.State.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < current.State.GetLength(1); j++)
+                        {
+                            Console.WriteLine(current.State[i, j]);
+                        }
+                    }
+                    break;
+                }
+
+                List<Node> nodes = current.GetChildren(current);
+
+                foreach (Node item in nodes)
+                {
+
+                    //nese e kena check niher state aktual ose gjendet ne queue e anashkalojm
+                    if (item.StateIsChecked(visited) || queue.Contains(item))
+                    {
+                        continue;
+                    }
+
+                    queue.Enqueue(item);
+                    visited.Add(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
